87872a421ffec7405e75b140441db62d7ac75e0c
Felipe Silva <felipefesilva19@gmail.com>
2025-07-12 14:03:05 -0300

    just some  corrections for deploying on vercel , nothing much

--- bakery-front/src/main.tsx
+++ bakery-front/src/main.tsx
@@ -1,5 +1,3 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BakeryContextProvider  } from './contexts/BakeryContext';
